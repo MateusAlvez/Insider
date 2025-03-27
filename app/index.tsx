2aa5988ae2ae04c6deb59bb800d1fc5369e0a726
Mateus Alvez <mateusrx1203@gmail.com>
2025-03-26 21:18:04 -0300

    Criando projeto

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View, SafeAreaView } from "react-native";
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Teste</Text>
+      </View>
+    </SafeAreaView>
+  );
+}

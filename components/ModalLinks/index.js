315985b6526146942dcaf59ee01a9355378f7d31
MatesAlvez <mateusrx1203@gmail.com>
2025-03-30 20:10:50 -0300

    S2Time21

---
+++ components/ModalLinks/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Text } from 'react-native';
+import { ModalContainer } from './styles';
+
+export default function ModalLinks()
+{
+    return(
+
+        <ModalContainer>
+
+            <Text> Teste Modal</Text>
+
+        </ModalContainer>
+
+
+    )
+
+
+}
--- src/Home/index.js
+++ src/Home/index.js
@@ -1,56 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
+import { Keyboard, KeyboardAvoidingView, Modal, Platform, TouchableWithoutFeedback } from "react-native";
+import ModalLinks from '../../components/ModalLinks/index';
 
 import { Feather } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
-import StatusBarPage from '../components/StatusBarPage';
-import { BoxIcon, ButtonLink, ButtonLinkText, ContainerContent, ContainerInput, ContainerLogo, Input, Logo, SubTitle, Title } from './style';
+import StatusBarPage from "../components/StatusBarPage";
+import {
+  BoxIcon,
+  ButtonLink,
+  ButtonLinkText,
+  ContainerContent,
+  ContainerInput,
+  ContainerLogo,
+  Input,
+  Logo,
+  SubTitle,
+  Title,
+} from "./style";
+
+
 
 export default function Home() {
 
-   return (  
-  <LinearGradient
-    colors={["#c7ede8", "#45b5c4"]}
-    style={{ flex: 1, justifyContent: "center" }}
-  >
-      <StatusBarPage 
-       barStyle= "light-content"  
-       backgroundColor = '#c7ede8'   
-      /> 
 
-    
-    <ContainerLogo>
-      <Logo source = { require('../../assets/images/logo.png')} 
-      resizeMode="contain"/>
-    </ContainerLogo>
+  const [input, setInput] = useState('');
 
-    <ContainerContent>
-       <Title>  
-         SujeitoLink
-        </Title>   
-        <SubTitle>
-          Cole seu link para encurtar. 
-        </SubTitle>
+  function handleShortLink()
+  {
+    //alert('URL DIGITADA: '+input)
+     
+  }
 
-        <ContainerInput>
-          <BoxIcon>
-            <Feather name="link" size={23} color="white"  />
-          </BoxIcon>
-         <Input
-          placeholder="Paste your link here..."
-          placeholderTextColor={'white'}
-         />
+  return (
+    <TouchableWithoutFeedback onPress={ () => Keyboard.dismiss()}>
+      <LinearGradient
+        colors={["#c7ede8", "#45b5c4"]}
+        style={{ flex: 1, justifyContent: "center" }}
+      >
+        <StatusBarPage barStyle="light-content" backgroundColor="#c7ede8" />
 
-      </ContainerInput>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          style={{ flex: 1 }}
+        >
+          <ContainerLogo>
+            <Logo
+              source={require("../../assets/images/logo.png")}
+              resizeMode="contain"
+            />
+          </ContainerLogo>
 
-      <ButtonLink>
-        <ButtonLinkText>Gerar text </ButtonLinkText>
-      </ButtonLink>
+          <ContainerContent>
+            <Title>SujeitoLink</Title>
+            <SubTitle>Cole seu link para encurtar.</SubTitle>
 
-    </ContainerContent>
+            <ContainerInput>
+              <BoxIcon>
+                <Feather name="link" size={23} color="white" />
+              </BoxIcon>
+              <Input
+                placeholder="Paste your link here..."
+                placeholderTextColor={"white"}
+                autoCapitalize="none"
+                autoCorrect={false}
+                keyboardType="url"
+                value={input}
+                onChangeText={(text) => setInput (text)}
+              />
+            </ContainerInput>
 
-      
-    
-  </LinearGradient>
+            <ButtonLink onPress={handleShortLink}>
+              <ButtonLinkText>Gerar texto</ButtonLinkText>
+            </ButtonLink>
+          </ContainerContent>
+        </KeyboardAvoidingView>
 
-   )
+        <Modal visible={true} transparent animationType="slide">
+
+          <ModalLinks/>
+
+        </Modal>
+
+
+      </LinearGradient>
+    </TouchableWithoutFeedback>
+  );
 }
